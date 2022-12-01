6df2e85dbcd664b27cf4c1dd7a54c75077bcf8f7
Your Name <you@example.com>
2022-12-01 14:15:51 +0500

    Sign in with google

--- app/src/main/java/com/example/chatapp/ChatsActivity.java
+++ app/src/main/java/com/example/chatapp/ChatsActivity.java
@@ -1,14 +1,69 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class ChatsActivity extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    ImageButton signOutBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
+
+        signOutBtn = findViewById(R.id.signout);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(ChatsActivity.this, gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(ChatsActivity.this);
+        if(acct != null){
+            UserInfo.user_name = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+
+            Toast toast = Toast.makeText(ChatsActivity.this, "Добро пожаловать " +  UserInfo.user_name, Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+
+        }
+
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    void signOut(){
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(Task<Void> task) {
+                finish();
+                startActivity(new Intent(ChatsActivity.this, MainActivity.class));
+            }
+        });
     }
 }
